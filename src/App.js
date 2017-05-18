2b1cb36cd5d4b25c4aabdf7f25dbe49b31879476
Sashko Stubailo <sashko@stubailo.com>
2017-05-18 02:43:44 +0200

    Add code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
+import { graphql, gql } from 'react-apollo';
+
 import './App.css';
 
 class App extends Component {
+  renderList() {
+    if (this.props.data.loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <ul>
+        {this.props.data.Repository.map(repo => (
+          <li>
+            <a href={repo.url}>{repo.full_name}</a>
+            :{' '}
+            {repo.favorites}
+            {' '}stars,{' '}
+            {repo.open_issues}
+            {' '}issues
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        Hello world!
+        <h1>Recently updated repositories:</h1>
+        {this.renderList()}
       </div>
     );
   }
 }
 
-export default App;
+// Write new queries here: http://graphql.communitygraph.org/graphiql/
+const query = gql`
+  {
+    Repository(orderBy: [updated_desc], first: 20) {
+      url
+      created
+      favorites
+      updated
+      full_name
+      homepage
+      language
+      score
+      open_issues
+      owner {
+        name
+      }
+    }
+  }
+`;
+
+export default graphql(query)(App);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface,
+} from 'react-apollo';
+
 import App from './App';
 import './index.css';
 
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://graphql.communitygraph.org/graphql/',
+  }),
+});
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
